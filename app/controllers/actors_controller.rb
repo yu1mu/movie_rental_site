418a3a7bf5f9abc8fa1740ee3e487ca7fa2a91c8
yu1mu <all0fus168@gmail.com>
2020-12-13 20:18:01 +0900

    modify actor search

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,7 @@
 class ActorsController < ApplicationController
     before_action :authenticate_user!, except: %i(index)
     def search
-        @results = Movie.ransack(actors_actor_name_or_movie_name_or_movie_description_or_movie_type_cont: params[:q]).result
+        @results = Movie.ransack(actors_actor_name_cont: params[:q]).result
         @appeared = AppearedIn.all
     end
 end
